7414c9987ba4297c72734f17330625a757cf95b4
runarer <95697611+runarer@users.noreply.github.com>
2025-12-29 10:27:54 +0100

    Adds Count and Items

--- src/Core/Interfaces/IStorable.cs
+++ src/Core/Interfaces/IStorable.cs
@@ -5,4 +5,6 @@ public interface IStorable<T>
     public void Add(T obj);
     public bool TryGet(int index, out T? obj);
     public void RemoveAt(int index);
+    public IEnumerable<T> Items {get;}
+    public int Count {get;}
 }
