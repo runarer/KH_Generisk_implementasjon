1eb1dcbfaf32f7d7fe66462a28343b6171ba855f
runarer <95697611+runarer@users.noreply.github.com>
2025-12-29 10:13:30 +0100

    Makes out T nullable

--- src/Core/Interfaces/IStorable.cs
+++ src/Core/Interfaces/IStorable.cs
@@ -3,6 +3,6 @@ namespace Core;
 public interface IStorable<T>
 {
     public void Add(T obj);
-    public bool TryGet(int index, out T obj);
+    public bool TryGet(int index, out T? obj);
     public void RemoveAt(int index);
 }
