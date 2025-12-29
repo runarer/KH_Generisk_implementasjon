9fea963d52060c307958c22342e6f6f7e4aa5102
runarer <95697611+runarer@users.noreply.github.com>
2025-12-29 10:24:06 +0100

    Make Count public

--- src/Core/Container.cs
+++ src/Core/Container.cs
@@ -3,18 +3,18 @@ namespace Core;
 public class Container<T> : IStorable<T> 
 {
     private List<T> _content = [];
-    private int _count = 0;
+    public int Count {get; private set; } = 0;
 
     public void Add(T obj)
     {
         _content.Add(obj);
-        ++_count;
+        ++Count;
     }
 
     public bool TryGet(int index, out T? obj)
     {
         obj = default(T);
-        if(index >= _count)
+        if(index >= Count)
             return false;
         obj = _content[index];
         return true;
@@ -23,6 +23,6 @@ public class Container<T> : IStorable<T>
     public void RemoveAt(int index)
     {
         _content.RemoveAt(index);
-        --_count;
+        --Count;
     }
 }
