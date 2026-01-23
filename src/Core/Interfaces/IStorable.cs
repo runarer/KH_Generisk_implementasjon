dd8e3b7d0053b3ac57f7dc6c5d7bdbc0d532f0c5
runarer <95697611+runarer@users.noreply.github.com>
2026-01-23 13:17:49 +0100

    Rearrange lines for clarity

--- src/Core/Interfaces/IStorable.cs
+++ src/Core/Interfaces/IStorable.cs
@@ -3,8 +3,10 @@ namespace Core;
 public interface IStorable<T>
 {
     public void Add(T obj);
-    public bool TryGet(int index, out T? obj);
+
     public void RemoveAt(int index);
+
+    public bool TryGet(int index, out T? obj);
 
     public bool TryGetLast(out T? obj);
 
