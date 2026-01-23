86b51a3e60300bd1daf52df442087e52fc33e1ba
runarer <95697611+runarer@users.noreply.github.com>
2026-01-23 12:26:49 +0100

    New tests for methods in interface

--- Tests/Core.Tests/StorableContractTests.cs
+++ Tests/Core.Tests/StorableContractTests.cs
@@ -40,5 +40,57 @@ public class StorableContractTests
         Assert.True(c.TryGet(1, out var value));
         Assert.Equal("C", value);
     }
+
+    [Fact]
+    public void TryGetLast_ReturnsLastElementAndTrue_AndSetLastToPreviousElement()
+    {
+        var c = new Container<string>();
+        string zero = "Index Zero";
+        string one = "Index One";
+        c.Add(zero);
+        c.Add(one);
+
+        bool ok = c.TryGetLast(out string? result);
+
+        Assert.True(ok);
+        Assert.Equal(one, result);
+        Assert.Equal(zero, c.Content.Last());
+    }
+
+    [Fact]
+    public void TryGetLast_NoElementsInStorage_ReturnsFalse()
+    {
+        var c = new Container<string>();
+
+        bool ok = c.TryGetLast(out string? _);
+
+        Assert.False(ok);
+    }
+
+    [Fact]
+    public void TryGetFirst_ReturnsFirstElementAndTrue_AndSeFirstToNextElement()
+    {
+        var c = new Container<string>();
+        string zero = "Index Zero";
+        string one = "Index One";
+        c.Add(zero);
+        c.Add(one);
+
+        bool ok = c.TryGetFirst(out string? result);
+
+        Assert.True(ok);
+        Assert.Equal(zero, result);
+        Assert.Equal(one, c.Content.First());
+    }
+
+    [Fact]
+    public void TryGetFirst_NoElementsInStorage_ReturnsFalse()
+    {
+        var c = new Container<string>();
+
+        bool ok = c.TryGetFirst(out string? _);
+
+        Assert.False(ok);
+    }
 }
 
